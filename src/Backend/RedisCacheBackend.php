9acf0826c27b1c7fd213f2878713d3736f66b8f8
Shani Elharrar <atnt123@bezeqint.net>
2012-08-17 15:54:45 +0300

    Added RedisCacheBackend

---
+++ src/Backend/RedisCacheBackend.php
@@ -0,0 +1,100 @@
+<?php
+namespace EggCup\Backend;
+use \EggCup\ICacheItem as ICacheItem;
+
+require_once '../../ext/predis/autoload.php';
+\Predis\Autoloader::register();
+
+/**
+ * Redis cache backend, for long running scalable php applications or
+ * short running php applications (Web sites, etc')
+ *
+ * @package default
+ * @author Shani Elharrar
+ **/
+class RedisCacheBackend implements ICacheBackend
+{
+	
+	private $_client;
+	
+	public function __construct(\Predis\Client $client) {
+		
+		$this->_client = $client;
+		
+	}
+	
+	public static function FormatKey($key) {
+		return '__KEY_' . $key;
+	}
+	
+	public static function FormatTag($tag) {
+		return '__TAG_' . $tag;
+	}
+	
+	/**
+	 * Caches the item with its settings
+	 * in the backend
+	 *
+	 * @return void
+	 **/
+	public function Cache(ICacheItem $item) {
+		
+		$client = $this->_client;
+				
+		$replies = $this->_client->pipeline(function($pipe) use ($item) {
+			
+			$ttl = $item->GetExpiryDate()->getTimestamp() - time();
+			$key = RedisCacheBackend::FormatKey($item->GetKey());
+			
+			$pipe->set($key, serialize($item->GetValue()));
+			$pipe->expire($key, $ttl);
+
+			foreach ($item->GetTags() as $tag) {
+				$pipe->sadd(RedisCacheBackend::FormatTag($tag), $key);
+			}
+			
+		});
+		
+	}
+	
+	/**
+	 * Removes all the cache items associated
+	 * with the given tag name
+	 *
+	 * @return void
+	 **/
+	public function DeleteByTag($tagName) {
+		
+		$tagFormatted = RedisCacheBackend::FormatTag($tagName);
+		$members = $this->_client->smembers($tagFormatted);
+		
+		$replies = $this->_client->pipeline(function ($pipe) use ($members, $tagFormatted) {
+		
+			$pipe->del($tagFormatted);
+		
+			foreach ($members as $member) {
+				$pipe->del($member);
+			}
+		
+		});
+		
+	}
+	
+	/**
+	 * Tries to get the value of an item 
+	 * by it's key
+	 *
+	 * @return bool
+	 **/
+	public function TryGetByKey($key, &$value) {
+		
+		$value = $this->_client->get(self::FormatKey($key));
+		
+		if ($value === null) {
+			return false;
+		}
+		
+		$value = unserialize($value);
+		return true;		
+	}
+} 
---
+++ src/Tests/Backend/RedisCacheBackendTests.php
@@ -0,0 +1,73 @@
+<?php
+use \pUnit\Assert as Assert;
+
+/**
+ * A tests class for RedisCacheBackend.
+ *
+ **/
+class RedisCacheBackendTests {
+	
+	private function GetCache() {
+		
+		return new \EggCup\Backend\RedisCacheBackend(new \Predis\Client(array('host' => '127.0.0.1', 'port' => 6379)));
+
+	}
+	
+	/**
+	 * Checks that getting a valid cache item
+	 * works well
+	 *
+	 * @return void
+	 **/
+	public function Cache_And_Get_By_Key(){ 
+		// Arrange
+		$cache = $this->GetCache();
+		$date = new DateTime();
+		$date = $date->add(new DateInterval("P1Y"));
+		$cacheItem = new \EggCup\CacheItem("key", $date, array(), "value");
+		// Act
+		$cache->Cache($cacheItem);
+		// Assert
+		Assert::IsTrue($cache->TryGetByKey("key", $value));
+		Assert::AreEqual($value, "value");
+	}
+	
+	/**
+	 * Checks that getting a
+	 * cached item that expired returns false
+	 *
+	 * @return void
+	 **/
+	public function Cache_And_Get_Expired() {
+		// Arrange
+		$cache = $this->GetCache();
+		$date = new DateTime();
+		$date = $date->sub(new DateInterval("P1Y"));
+		$cacheItem = new \EggCup\CacheItem("key", $date, array(), "value");
+		// Act
+		$cache->Cache($cacheItem);
+		// Assert
+		Assert::IsFalse($cache->TryGetByKey("key", $value));
+	}
+	
+	/**
+	 * Checks that caching an item
+	 * and unvalidating it's tag 
+	 * removes the item.
+	 *
+	 * @return void
+	 **/
+	public function Cache_And_Remove_Tags() {
+		// Arrange
+		$cache = $this->GetCache();
+		$date = new DateTime();
+		$date = $date->add(new DateInterval("P1Y"));
+		$cacheItem = new \EggCup\CacheItem("key", $date, array("MyTag"), "value");
+		$cache->Cache($cacheItem);
+		// Act
+		$cache->DeleteByTag("MyTag");
+		// Assert
+		Assert::IsFalse($cache->TryGetByKey("key", $value));	
+	}
+	
+}
